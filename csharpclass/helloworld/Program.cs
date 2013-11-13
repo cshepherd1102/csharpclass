9ac5bae50d40dc4e0084fc14f1bef7e4c6aadabd
chris shepherd <chrisshepherd@tnwinc.com>
2013-11-13 14:09:19 -0800

    hello world program

---
+++ csharpclass/helloworld/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var doIt = 0;
+            var greeting = "Hellow World!";
+
+            switch (doIt)
+            {
+
+                case 0:
+                    Console.WriteLine(greeting);
+                    break;
+
+                case 1:
+
+                    Console.WriteLine("Goodbye...ruel world!!");
+                    break;
+
+                default:
+                    Console.WriteLine("fallthrough...");
+                    break;
+            }
+        }
+    }
+}
---
+++ csharpclass/helloworld/foreach.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace echo
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            //create an array  - knows it size...unlike a list which can grow and shrink
+            var attendees = new[] { "mike", "Mark", "Mark", "Jacob" };
+            
+            foreach(var name in attendees)
+            {
+                Console.WriteLine(name);
+
+            }
+
+            var i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine("try and stop me! (i = " +  i++ + ")");
+                //i++;
+                //i = i + 1;   These are equivalent
+                //i += 1;
+            }
+            
+           
+        }
+    }
+}
